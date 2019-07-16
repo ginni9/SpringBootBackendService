256b1da7770961a8b7f2ab19fd242d137636f4dd
Gagandeep <gagandeepkaur1895@gmail.com>
2019-07-17 02:55:02 +0530

    Downloaded Skeleton from Spring Intializr

---
+++ src/main/java/com/NITKKR/gagandeepKaur/SpringBootBackendService/SpringBootBackendServiceApplication.java
@@ -0,0 +1,12 @@
+package com.NITKKR.gagandeepKaur.SpringBootBackendService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendServiceApplication.class, args);
+	}
+}
